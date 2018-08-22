f130eb8d041a84760918b87447c9c61fb77c6bff
ArakiMasashi <masa121codbf@gmail.com>
2018-08-22 14:18:00 +0900

    fix：ItemCategoryDataBeans追加に伴い、categoryの変数の型を変更

--- Project/src/deans/ItemDataBeans.java
+++ Project/src/deans/ItemDataBeans.java
@@ -3,7 +3,7 @@ package deans;
 import java.io.Serializable;
 
 /**
- * アイテム
+ * 商品情報
  * @author 31510007-PC
  *
  */
@@ -12,8 +12,8 @@ public class ItemDataBeans implements Serializable {
 	private int id;
 	private int price;
 	private int count;
+	private int category;
 	private String name;
-	private String category;
 	private String detail;
 	private String fileName;
 
@@ -24,44 +24,56 @@ public class ItemDataBeans implements Serializable {
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public int getCount() {
 		return count;
 	}
+
 	public void setCount(int count) {
 		this.count = count;
 	}
+
+	public int getCategory() {
+		return category;
+	}
+
+	public void setCategory(int category) {
+		this.category = category;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
+
 	public String getDetail() {
 		return detail;
 	}
+
 	public void setDetail(String detail) {
 		this.detail = detail;
 	}
+
 	public String getFileName() {
 		return fileName;
 	}
+
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
-
 }
