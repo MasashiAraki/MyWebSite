57890db8ef4213af23e29e980d7e4a7e795088ad
ArakiMasashi <masa121codbf@gmail.com>
2018-08-26 01:25:44 +0900

    update：DAOを追加

---
+++ Project/src/dao/ItemDAO.java
@@ -0,0 +1,99 @@
+package dao;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import base.DBManager;
+import deans.ItemDataBeans;
+
+public class ItemDAO {
+
+	/**
+	 * 全てのアイテム情報
+	 * @return
+	 */
+	public static List<ItemDataBeans> getAllItem(){
+		Connection con = null;
+		List<ItemDataBeans> itemList = new ArrayList<ItemDataBeans>();
+
+		try {
+			con = DBManager.getConnection();
+			String sql = "SELECT * FROM m_item";
+
+			Statement stmt = (Statement) con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				ItemDataBeans idb = new ItemDataBeans();
+				idb.setId(rs.getInt("id"));
+				idb.setPrice(rs.getInt("price"));
+				idb.setCount(rs.getInt("count"));
+				idb.setName(rs.getString("name"));
+				idb.setCategory(rs.getString("category"));
+				idb.setDetail(rs.getString("detail"));
+				idb.setFileName(rs.getString("file_name"));
+				itemList.add(idb);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					return null;
+				}
+			}
+		}
+		return itemList;
+	}
+
+
+	/**
+	 * 商品IDに該当する商品情報を取得
+	 * @param id 商品ID
+	 * @return
+	 */
+	public static ItemDataBeans getItem (int id) {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement("SELECT * FROM m_item WHERE id = ?");
+			pStmt.setInt(1, id);
+			ResultSet rs = pStmt.executeQuery();
+
+			if (rs.next()) {
+				ItemDataBeans idb = new ItemDataBeans();
+				idb.setId(rs.getInt("id"));
+				idb.setPrice(rs.getInt("price"));
+				idb.setCount(rs.getInt("count"));
+				idb.setName(rs.getString("name"));
+				idb.setCategory(rs.getString("category"));
+				idb.setDetail(rs.getString("detail"));
+				idb.setFileName(rs.getString("file_name"));
+				return idb;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+
+}
---
+++ Project/src/dao/UserDAO.java
@@ -0,0 +1,126 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import base.Common;
+import base.DBManager;
+import deans.UserDataBeans;
+
+public class UserDAO {
+
+	/**
+	 * ユーザ情報の登録
+	 * @param udb ユーザ情報を保持しているBeans
+	 * @throws SQLException
+	 */
+	public static void InsertUser(UserDataBeans udb) {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement("INSERT INTO m_user(name,postcode,address,login_id,login_password,create_date) VALUES(?,?,?,?,?,?)");
+			pStmt.setString(1, udb.getName());
+			pStmt.setInt(2, udb.getPostcode());
+			pStmt.setString(3, udb.getAddress());
+			pStmt.setString(4, udb.getLoginId());
+			pStmt.setString(5, Common.encryption(udb.getPassword()));
+			pStmt.setTimestamp(6, new Timestamp(System.currentTimeMillis()));
+			pStmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("失敗");
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * ログインIDの重複を調べる
+	 * @param loginId
+	 * @return 重複があればtrue
+	 */
+	public static boolean isOverlapLoginId(String loginId) {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement("SELECT login_id FROM m_user WHERE login_id = ?");
+			pStmt.setString(1, loginId);
+			ResultSet rs = pStmt.executeQuery();
+			if (rs.next()) {
+				return true;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 *  ユーザ情報の有無（ログインで使用）
+	 * @param loginId
+	 * @param password
+	 * @return ユーザ情報があればtrue
+	 */
+	public static UserDataBeans getUserInfo (String loginId1, String password) {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement("SELECT * FROM m_user WHERE login_id = ? and login_password = ?");
+			pStmt.setString(1, loginId1);
+			pStmt.setString(2, Common.encryption(password));
+			ResultSet rs = pStmt.executeQuery();
+
+			if (!rs.next()) {
+				return null;
+			}
+
+			UserDataBeans udb = new UserDataBeans();
+			udb.setId(rs.getInt("id"));
+			udb.setPostcode(rs.getInt("postcode"));
+			udb.setName(rs.getString("name"));
+			udb.setAddress(rs.getString("address"));
+			udb.setLoginId(rs.getString("login_id"));
+			return udb;
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+
+	}
+
+}
