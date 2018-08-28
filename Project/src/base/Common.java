2885e316b66c4b970427569255ccd0e2be04eccc
ArakiMasashi <masa121codbf@gmail.com>
2018-08-28 15:09:30 +0900

    add：共通のクラスメソッドを追加

---
+++ Project/src/base/Common.java
@@ -0,0 +1,59 @@
+package base;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import deans.ItemDataBeans;
+
+public class Common {
+
+	/**
+	 * パスワードを暗号化
+	 * @param target 入力したパスワード
+	 * @return 暗号化したパスワード
+	 */
+	public static String encryption(String target) {
+		MessageDigest md = null;
+		StringBuffer buf = new StringBuffer();
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+			md.update(target.getBytes());
+			byte[] digest = md.digest();
+
+			for (int i = 0; i < digest.length; i++) {
+				buf.append(String.format("%02x", digest[i]));
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return buf.toString();
+	}
+
+	/**
+	 * 商品の合計金額を算出する
+	 * @param cartItem
+	 * @return total
+	 */
+	public static int getTotalItemPrice(List<ItemDataBeans> cartItem) {
+		int total = 0;
+		for (ItemDataBeans item : cartItem) {
+			total += item.getPrice();
+		}
+		return total;
+	}
+
+	/**
+	 * ログイン前のページを取得
+	 * @param session
+	 * @param str
+	 * @return
+	 */
+	public static Object cutSessionAttribute(HttpSession session, String str) {
+		Object test = session.getAttribute(str);
+		session.removeAttribute(str);
+		return test;
+	}
+}
