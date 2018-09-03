95aa4594d8f0c7240703f6f8253cdbf5c33621da
ArakiMasashi <masa121codbf@gmail.com>
2018-09-03 23:25:32 +0900

    add：複数のDAO追加

--- Project/src/dao/ItemDAO.java
+++ Project/src/dao/ItemDAO.java
@@ -101,4 +101,170 @@ public class ItemDAO {
 		return null;
 	}
 
+	/**
+	 * 商品情報を更新する
+	 * @param idb
+	 * @throws SQLException
+	 */
+	public static void updateItem(ItemDataBeans idb) throws SQLException {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement("UPDATE m_item SET name=?, category_id=?, detail=?, price=? WHERE id=?;");
+			pStmt.setString(1, idb.getName());
+			pStmt.setInt(2, idb.getCategoryId());
+			pStmt.setString(3, idb.getDetail());
+			pStmt.setInt(4, idb.getPrice());
+			pStmt.setInt(5, idb.getId());
+
+			pStmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	/**
+	 * 商品IDに該当する商品レコードを削除
+	 * @param id
+	 * @throws SQLException
+	 */
+	public static void deleteItem(int id) throws SQLException {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement("DELETE FROM m_item WHERE id=?;");
+			pStmt.setInt(1, id);
+
+			pStmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	/**
+	 * 商品を新規登録する
+	 * @param idb
+	 * @throws SQLException
+	 */
+	public static void insertItem(ItemDataBeans idb) throws SQLException {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement("INSERT INTO m_item (name,category_id,detail,price,file_name) VALUE (?,?,?,?,?)");
+			pStmt.setString(1, idb.getName());
+			pStmt.setInt(2, idb.getCategoryId());
+			pStmt.setString(3, idb.getDetail());
+			pStmt.setInt(4, idb.getPrice());
+			pStmt.setString(5, idb.getFileName());
+			pStmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	/**
+	 * カテゴリーIDと一致する全ての商品を取得
+	 * @param categoryId
+	 * @return itemList<itemDataBeans> 商品のリスト
+	 * @throws SQLException
+	 */
+	public static List<ItemDataBeans> getItemByCategoryId(int categoryId) throws SQLException {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		List<ItemDataBeans> itemList = new ArrayList<ItemDataBeans>();
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement(
+					"SELECT * FROM m_item"
+							+ " JOIN m_item_category"
+							+ " ON m_item.category_id = m_item_category.id"
+							+ " WHERE m_item.category_id = ?");
+			pStmt.setInt(1, categoryId);
+			ResultSet rs = pStmt.executeQuery();
+
+			while(rs.next()) {
+				ItemDataBeans idb = new ItemDataBeans();
+				idb.setId(rs.getInt("id"));
+				idb.setName(rs.getString("name"));
+				idb.setCategoryId(rs.getInt("category_id"));
+				idb.setDetail(rs.getString("detail"));
+				idb.setPrice(rs.getInt("price"));
+				idb.setFileName(rs.getString("file_name"));
+				itemList.add(idb);
+			}
+			return itemList;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	/**
+	 * 商品名を検索
+	 * @param word 検索ワード
+	 * @return itemList<ItemDataBeans> 検索ワードに該当する商品のリスト
+	 * @throws SQLException
+	 */
+	public static List<ItemDataBeans> searchItem(String word) throws SQLException {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		List<ItemDataBeans> itemList = new ArrayList<ItemDataBeans>();
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement(
+					"SELECT * FROM m_item"
+							+ " JOIN m_item_category"
+							+ " ON m_item.category_id = m_item_category.id"
+							+ " WHERE name collate utf8_unicode_ci LIKE ?");
+			pStmt.setString(1, "%" + word + "%");
+			ResultSet rs = pStmt.executeQuery();
+
+			while(rs.next()) {
+				ItemDataBeans idb = new ItemDataBeans();
+				idb.setId(rs.getInt("id"));
+				idb.setPrice(rs.getInt("price"));
+				idb.setName(rs.getString("name"));
+				idb.setCategoryId(rs.getInt("category_id"));
+				idb.setDetail(rs.getString("detail"));
+				idb.setFileName(rs.getString("file_name"));
+				idb.setCategoryName(rs.getString("category_name"));
+				itemList.add(idb);
+			}
+			return itemList;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+
 }
--- Project/src/dao/UserDAO.java
+++ Project/src/dao/UserDAO.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 import base.Common;
 import base.DBManager;
@@ -120,7 +123,205 @@ public class UserDAO {
 			}
 		}
 		return null;
-
 	}
 
+
+	/**
+	 * ユーザ情報を更新する
+	 * @param udb 入力されたユーザ情報のBeans
+	 * @throws SQLException
+	 */
+	public static void updateUser(UserDataBeans udb) throws SQLException {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement("UPDATE m_user SET name=?, postcode=?, address=?, login_password=? WHERE id=?;");
+			pStmt.setString(1, udb.getName());
+			pStmt.setInt(2, udb.getPostcode());
+			pStmt.setString(3, udb.getAddress());
+			pStmt.setString(4, Common.encryption(udb.getPassword()));
+			pStmt.setInt(5, udb.getId());
+			pStmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	/**
+	 * 全てのユーザ情報を取得
+	 * @return ユーザ情報のリスト
+	 * @throws SQLException
+	 */
+	public static List<UserDataBeans> getAllUserInfo() throws SQLException {
+		Connection con = null;
+		List<UserDataBeans> userList = new ArrayList<UserDataBeans>();
+		try {
+			con = DBManager.getConnection();
+			String sql = "SELECT * FROM m_user";
+
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				UserDataBeans udb = new UserDataBeans();
+				udb.setId(rs.getInt("id"));
+				udb.setName(rs.getString("name"));
+				udb.setPostcode(rs.getInt("postcode"));
+				udb.setAddress(rs.getString("address"));
+				udb.setLoginId(rs.getString("login_id"));
+				udb.setCreateDate(rs.getDate("create_date"));
+				userList.add(udb);
+			}
+			return userList;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	/**
+	 * idをもとにユーザ情報を検索
+	 * @param id
+	 * @return udb idと一致するユーザ情報
+	 * @throws SQLException
+	 */
+	public static UserDataBeans getUserInfoBuyId(int id) throws SQLException {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement("SELECT * FROM m_user WHERE id = ?");
+			pStmt.setInt(1, id);
+			ResultSet rs = pStmt.executeQuery();
+
+			UserDataBeans udb = new UserDataBeans();
+			if (rs.next()) {
+				udb.setId(rs.getInt("id"));
+				udb.setName(rs.getString("name"));
+				udb.setPostcode(rs.getInt("postcode"));
+				udb.setAddress(rs.getString("address"));
+				udb.setLoginId(rs.getString("login_id"));
+				udb.setCreateDate(rs.getDate("create_date"));
+			}
+			return udb;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	/**
+	 * idをもとにユーザ情報を削除
+	 * @param id ユーザID
+	 * @throws SQLException
+	 */
+	public static void deleteUser(int id) throws SQLException {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement("DELETE FROM m_user WHERE id = ?");
+			pStmt.setInt(1, id);
+			pStmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	/**
+	 * ログインIDを検索して取得
+	 * @param word 検索ワード
+	 * @return userList<UserDataBeans> 検索ワードに一致するユーザ情報のリスト
+	 * @throws SQLException
+	 */
+	public static List<UserDataBeans> searchUserByLoginId(String word) throws SQLException {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		List<UserDataBeans> userList = new ArrayList<UserDataBeans>();
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement(
+					"SELECT * FROM m_user WHERE login_id collate utf8_unicode_ci LIKE ?");
+			pStmt.setString(1, "%" + word + "%");
+			ResultSet rs = pStmt.executeQuery();
+
+			while(rs.next()) {
+				UserDataBeans udb = new UserDataBeans();
+				udb.setId(rs.getInt("id"));
+				udb.setName(rs.getString("name"));
+				udb.setPostcode(rs.getInt("postcode"));
+				udb.setAddress(rs.getString("address"));
+				udb.setLoginId(rs.getString("login_id"));
+				udb.setCreateDate(rs.getDate("create_date"));
+				userList.add(udb);
+			}
+			return userList;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	/**
+	 * 名前を検索して取得
+	 * @param word 検索ワード
+	 * @return userList<UserDataBeans> 検索ワードに一致するユーザ情報のリスト
+	 * @throws SQLException
+	 */
+	public static List<UserDataBeans> searchUserByUserName(String word) throws SQLException {
+		Connection con = null;
+		PreparedStatement pStmt = null;
+		List<UserDataBeans> userList = new ArrayList<UserDataBeans>();
+		try {
+			con = DBManager.getConnection();
+			pStmt = con.prepareStatement(
+					"SELECT * FROM m_user WHERE name collate utf8_unicode_ci LIKE ?");
+			pStmt.setString(1, "%" + word + "%");
+			ResultSet rs = pStmt.executeQuery();
+
+			while(rs.next()) {
+				UserDataBeans udb = new UserDataBeans();
+				udb.setId(rs.getInt("id"));
+				udb.setName(rs.getString("name"));
+				udb.setPostcode(rs.getInt("postcode"));
+				udb.setAddress(rs.getString("address"));
+				udb.setLoginId(rs.getString("login_id"));
+				udb.setCreateDate(rs.getDate("create_date"));
+				userList.add(udb);
+			}
+			return userList;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
 }
